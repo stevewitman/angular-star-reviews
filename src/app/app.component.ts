8905f521a5b60cb84bb818b8c5dc20c84b7024bf
Steve Witman <stevewitman@gmail.com>
2017-10-26 23:20:42 -0600

    Set up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  userDoc: AngularFirestoreDocument<any>;
+  movieDoc: AngularFirestoreDocument<any>;
+
+  user: Observable<any>;
+  movie: Observable<any>;
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.userDoc = this.afs.doc('users/test-user-3');
+    this.movieDoc = this.afs.doc('movies/battfield-earth');
+
+    this.movie = this.movieDoc.valueChanges();
+    this.user = this.userDoc.valueChanges();
+  }
+
+  get movieId() {
+    return this.movieDoc.ref.id;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
